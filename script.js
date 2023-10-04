f1ab960998a0a2185795930e6d402411ca4bd670
Alumoys <128159774+Alumoys@users.noreply.github.com>
2023-10-04 12:05:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const counterElement = document.querySelector(".counter");
+const barElement = document.querySelector(".loanding-bar-front");
+
+let idx = 0;
+updateNum();
+
+function updateNum(){
+    counterElement.innerHTML = idx + "%";
+    barElement.style.width = idx + "%";
+    idx ++;
+    if(idx < 101){
+        setTimeout(updateNum, 20);
+    }
+}
